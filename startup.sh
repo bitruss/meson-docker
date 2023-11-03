65ce9de81dc140e5779d0b893bc29be77e95b90e
bitruss <bitruss@protonmail.com>
2023-11-03 15:05:39 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Set Meson CDN configuration
-meson_cdn config set --token=$TOKEN --https_port=$HTTPS_PORT 
 
+service install meson_cdn
+meson_cdn config set --token=$TOKEN --https_port=$HTTPS_PORT 
 service start meson_cdn
